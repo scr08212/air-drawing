15692800c376a366acf1af1ee18344a0613ea729
NKM <scr08212@gmail.com>
2025-06-20 02:15:10 +0900

    Update Stroke.java
    주석 변경

--- app/src/main/java/com/nkm/capstone/air_drawing/data/Stroke.java
+++ app/src/main/java/com/nkm/capstone/air_drawing/data/Stroke.java
@@ -12,14 +12,18 @@ import android.util.Pair;
  * 연속적인 선 궤적을 저장하는 데이터 클래스입니다.
  *
  * <br>
- * - 각 Stroke는 PointF 리스트로 구성되며, 선을 구성하는 순차적인 좌표들을 포함합니다.
- * - DrawView에서 터치 구간마다 하나의 Stroke 인스턴스를 생성하여 분리된 선을 관리합니다.
- * - 이를 통해 터치 중단 구간을 구분하고, 자연스러운 라인 렌더링이 가능합니다.
+ * <b>기능 설명:</b>
+ * <ul>
+ *     <li>각 Stroke는 PointF 객체의 리스트로 구성되며, 그 순서에 따라 연속적인 궤적을 나타냅니다.</li>
+ *     <li>깊은 복사 생성자(복사 생성자)를 제공하여 기존 Stroke를 안전하게 복제할 수 있습니다.</li>
+ *     <li>{@code lineSegments()}를 통해 선분 쌍(Pair<PointF, PointF>)을 iterable 형태로 반환하여
+ *     Canvas에 선을 쉽게 그릴 수 있도록 지원합니다.</li>
+ *     <li>DrawView 등에서 사용자가 그린 경로를 분리, 저장, 렌더링하는 데 사용됩니다.</li>
+ * </ul>
  *
  * @author 남경민
- * @since 2025.06.15
+ * @since 2025.06.20
  */
-
 
 public class Stroke
 {
